04db1870b6a3ce14e498a8a2a432eeeeaa59ef8a
hamedes <hamed.es1990april@gmail.com>
2024-07-30 06:38:24 -0700

    two

--- scripts.js
+++ scripts.js
@@ -1,16 +1,36 @@
+var today=12
+var day;
 
 
+//console.log(today);
 
 
-var a,b;
-var isEqual;
- a=10;
- b=5;
-
-
-
-a==b ? isEqual=true  :  isEqual=false;
-
- console.log("the equal match is:"+isEqual)
-
-
+switch(today)
+{
+    case 0:
+           day ="sunday";
+           break;
+    case 1:
+            day ="monday";
+            break;
+    case 2:
+            day ="tuesday";
+            break;
+    case 3:
+            day ="wendsday";
+            break; 
+    case 4:
+            day ="thursday";
+            break; 
+    case 5:
+            day ="friday";
+            break;
+    case 6:
+            day ="saturday";
+            break;
+            default:
+                day="your value not fond";
+            
+}
+//console.log("today is:"+day);
+document.write("today is:"+day);
